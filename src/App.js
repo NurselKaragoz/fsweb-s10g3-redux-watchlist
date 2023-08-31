f45dd5387966890879bfed7019020fcfd26f407f
Nursel Karagoz <nurselkaragoz@outlook.com>
2023-08-31 11:10:48 +0300

    önceki ve sonraki butonlar eklendi

--- src/App.js
+++ src/App.js
@@ -2,22 +2,38 @@ import { useState } from "react";
 import { Switch, Route, NavLink } from "react-router-dom";
 import Movie from "./components/Movie";
 import FavMovie from "./components/FavMovie";
+import { useDispatch, useSelector } from "react-redux";
+import { SONRAKI_FILM, ONCEKI_FILM } from "./store/actions";
 
 function App() {
-  const [sira, setSira] = useState(0);
-  const favMovies = [];
+  // const [sira, setSira] = useState(0);
+  const sira = useSelector((store) => store.sira);
+  const dispatch = useDispatch();
+
+  const favMovies = useSelector((store) => store.favoriFilmler);
 
   function sonrakiFilm() {
-    setSira(sira + 1);
+    dispatch({ type: SONRAKI_FILM });
   }
-
+  function oncekiFilm() {
+    dispatch({ type: ONCEKI_FILM });
+  }
   return (
     <div className="wrapper max-w-2xl mx-auto">
       <nav className="flex text-2xl pb-6 pt-8 gap-2 justify-center">
-        <NavLink to="/" exact className="py-3 px-6 " activeClassName="bg-white shadow-sm text-blue-600">
+        <NavLink
+          to="/"
+          exact
+          className="py-3 px-6 "
+          activeClassName="bg-white shadow-sm text-blue-600"
+        >
           Filmler
         </NavLink>
-        <NavLink to="/listem" className="py-3 px-6 " activeClassName="bg-white shadow-sm text-blue-600">
+        <NavLink
+          to="/listem"
+          className="py-3 px-6 "
+          activeClassName="bg-white shadow-sm text-blue-600"
+        >
           Listem
         </NavLink>
       </nav>
@@ -30,7 +46,13 @@ function App() {
               onClick={sonrakiFilm}
               className="select-none px-4 py-2 border border-blue-700 text-blue-700 hover:border-blue-500 hover:text-blue-500"
             >
-              Sıradaki
+              Sonraki Film
+            </button>
+            <button
+              onClick={oncekiFilm}
+              className="select-none px-4 py-2 border border-blue-700 text-blue-700 hover:border-blue-500 hover:text-blue-500"
+            >
+              Önceki Film
             </button>
             <button className="select-none px-4 py-2 bg-blue-700 hover:bg-blue-600 text-white">
               Listeme ekle
---
+++ src/store/actions.js
@@ -0,0 +1,2 @@
+export const SONRAKI_FILM = "Sıradaki geç";
+export const ONCEKI_FILM = "Sıradaki geç";
